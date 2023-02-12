19901b44635559bfbe79cbb9463999b9bd565cf4
sushant <borkar.sushant3@gmail.com>
2023-02-12 18:34:45 +0530

    spring setup

---
+++ src/main/java/com/project/instaclone/InstacloneApplication.java
@@ -0,0 +1,13 @@
+package com.project.instaclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstacloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstacloneApplication.class, args);
+	}
+
+}
