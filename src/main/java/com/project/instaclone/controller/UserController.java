b0d1f4b03a39423f6eb0b804b0952af5c8bbbd1c
sushant <borkar.sushant3@gmail.com>
2023-03-03 00:28:07 +0530

    PutMapping needs to be fixed

--- src/main/java/com/project/instaclone/controller/UserController.java
+++ src/main/java/com/project/instaclone/controller/UserController.java
@@ -1,35 +1,104 @@
 package com.project.instaclone.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.Optional;
 
-import com.project.instaclone.Entity.Users;
-import com.project.instaclone.service.UserService;
+import javax.validation.Valid;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.instaclone.Entity.User;
+import com.project.instaclone.common.Response;
+import com.project.instaclone.service.UserService;
+
+import lombok.AllArgsConstructor;
 
 @RestController
 @RequestMapping("/users")
+@AllArgsConstructor
 public class UserController {
 
-    @Autowired
     UserService userService;
 
-    @PostMapping(value = "")
-    private boolean submitUser(@RequestBody Users users) {
-
-        return true;
-
+    @PostMapping()
+    private Response<User> submitUser(@RequestBody @Valid User user) {
+        Response<User> response = new Response<>();
+        try {
+            User savedUser = userService.saveUser(user);
+            if (savedUser != null) {
+                response.setData(savedUser);
+                response.setResponseCode(HttpStatus.CREATED.value());
+                response.setResponseMessage("User added successfully");
+                response.setStatus(HttpStatus.CREATED);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.setData(null);
+            response.setResponseCode(HttpStatus.BAD_REQUEST.value());
+            response.setResponseMessage("Error Occurred!");
+            response.setStatus(HttpStatus.BAD_REQUEST);
+        }
+        return response;
     }
 
-    @GetMapping("/{userid}")
-    private Users getUserDetails(@PathVariable("userid") String userId) {
+    @GetMapping("/{id}")
+    private Response<User> getUserDetails(@PathVariable("id") @Valid Integer id) {
+        Response<User> response = new Response<>();
+        try {
+            Optional<User> user = userService.getUserById(id);
+            if (user.isPresent()) {
+                response.setData(user.get());
+                response.setResponseCode(HttpStatus.OK.value());
+                response.setStatus(HttpStatus.OK);
+                response.setResponseMessage("User found for User ID: " + id);
+            } else {
+                response.setData(null);
+                response.setResponseCode(HttpStatus.NOT_FOUND.value());
+                response.setStatus(HttpStatus.NOT_FOUND);
+                response.setResponseMessage("User not found for User ID: " + id);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.setData(null);
+            response.setResponseCode(HttpStatus.BAD_REQUEST.value());
+            response.setResponseMessage("Error Occurred!");
+            response.setStatus(HttpStatus.BAD_REQUEST);
+        }
+        return response;
+    }
 
-        return new Users();
+    @PutMapping("/{id}")
+    public Response<User> updateUser(@PathVariable("id") @Valid Integer id) {
+        Response<User> response = new Response<>();
+        try {
+            Optional<User> user = userService.updateUser(id);
+            if (user.isPresent()) {
+                response.setData(user.get());
+                response.setResponseCode(HttpStatus.OK.value());
+                response.setStatus(HttpStatus.OK);
+                response.setResponseMessage("User Updated successfully for ID: " + id);
+            } else {
+                response.setData(null);
+                response.setResponseCode(HttpStatus.NOT_FOUND.value());
+                response.setStatus(HttpStatus.NOT_FOUND);
+                response.setResponseMessage("User not found for User ID: " + id);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.setData(null);
+            response.setResponseCode(HttpStatus.BAD_REQUEST.value());
+            response.setStatus(HttpStatus.BAD_REQUEST);
+            response.setResponseMessage("Error Occurred!");
+
+        }
+        return response;
     }
 
 }
